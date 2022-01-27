f18c23d5b1c3951a4d4c8add535765e3274ed04f
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-01-27 04:50:52 +0000

    odd & even code

---
+++ odd & even.js
@@ -0,0 +1,14 @@
+
+function isodd(x){
+  
+  return x%2===0? false:true;
+}
+
+console.log("3 is odd: " + isodd(4));
+
+// const isEven  = function (num) {
+//   return num % 2 === 0;
+// }
+
+// console.log(isEven(10));
+// console.log(isEven(11));
